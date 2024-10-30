30423291dd854070f80b02ddf06a8b9517eb4101
coffebara <qwshpke@naver.com>
2024-10-31 08:14:43 +0900

    first commit

---
+++ src/main/java/com/solomonm/autoChkHP/AutoChkHpApplication.java
@@ -0,0 +1,13 @@
+package com.solomonm.autoChkHP;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutoChkHpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutoChkHpApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/solomonm/autoChkHP/AutoChkHpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.solomonm.autoChkHP;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutoChkHpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
